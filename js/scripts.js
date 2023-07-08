e5749bf86cc5f2a48ed5bd9a8df49348d72a113c
kamal-maverick <kamal@mavericksoft.co.nz>
2023-07-08 17:47:01 +0530

    advanced form validation

--- js/scripts.js
+++ js/scripts.js
@@ -253,3 +253,14 @@ var areaChart = new ApexCharts(
 areaChart.render();
 
 
+//  ------------------ Profile ---------------------------
+
+let card = document.querySelector(".card"); //declearing profile card element
+let displayPicture = document.querySelector(".picture"); //declearing profile picture
+
+displayPicture.addEventListener("click", function() { //on click on profile picture toggle hidden class from css
+card.classList.toggle("hidden")})
+
+
+// -------------------------------------------
+
